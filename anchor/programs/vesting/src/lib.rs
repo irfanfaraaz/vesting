878cf68e4ecd9ad3d37c8352b036118060b0b7b0
irfanfaraaz <irfanfaraaz03@gmail.com>
2025-11-14 18:54:14 +0530

    refactor: update vesting program address and remove unused instructions and accounts from generated files

--- anchor/programs/vesting/src/lib.rs
+++ anchor/programs/vesting/src/lib.rs
@@ -1,70 +1,11 @@
 #![allow(clippy::result_large_err)]
+#![allow(unexpected_cfgs)]
 
 use anchor_lang::prelude::*;
 
-declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
+declare_id!("5wyZo4T5KuHZzFNFjZrBQTLgKK4CYKyMJ3Rq9HMUZJnF");
 
 #[program]
 pub mod vesting {
     use super::*;
-
-    pub fn close(_ctx: Context<CloseVesting>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn decrement(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.vesting.count = ctx.accounts.vesting.count.checked_sub(1).unwrap();
-        Ok(())
-    }
-
-    pub fn increment(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.vesting.count = ctx.accounts.vesting.count.checked_add(1).unwrap();
-        Ok(())
-    }
-
-    pub fn initialize(_ctx: Context<InitializeVesting>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-        ctx.accounts.vesting.count = value.clone();
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct InitializeVesting<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  init,
-  space = 8 + Vesting::INIT_SPACE,
-  payer = payer
-    )]
-    pub vesting: Account<'info, Vesting>,
-    pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVesting<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-    )]
-    pub vesting: Account<'info, Vesting>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub vesting: Account<'info, Vesting>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Vesting {
-    count: u8,
 }
